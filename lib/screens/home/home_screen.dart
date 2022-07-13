7cbae21eacef713deb457063c4fffc0834b17596
Dinislam <dyanursaev@gmail.com>
2022-07-13 16:24:03 +0300

    Initial commit

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,11 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
